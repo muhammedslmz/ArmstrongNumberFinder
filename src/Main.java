eb700818292bc1b977bec6056683727a7e6c88b6
Muhammed Solmaz <muhammed.slmz@gmail.com>
2022-12-13 21:15:54 +0300

    Calculator working well on digits.

--- src/Main.java
+++ src/Main.java
@@ -4,41 +4,27 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int numOfDigits=0;
-        Scanner input=new Scanner(System.in);
-        int num =input.nextInt();
-        int tempnum=num;
-        int stepValue;
-        int stepExponent;
+        int num;
+        int stepValue,numOfStep=0;
         int result=0;
-        while (tempnum!=0)
-        {
-            tempnum/=10;
-            numOfDigits++;
-        }
+       Scanner inp =new Scanner(System.in);
+       num= inp.nextInt();
+       int tempnum=num;
+       while(tempnum !=0)
+       {
+           tempnum/=10;
+           numOfStep++;
+       }
 
         tempnum=num;
-        while (tempnum!=0)
-        {
 
-            stepValue= tempnum%10;
-            stepExponent=1;
-            for(int i=1; i<=numOfDigits;i++)
-            {
-                stepExponent*=stepValue;
-            }
-            result+=stepExponent;
+        for(int i =1; i<=numOfStep;i++)
+        {
+            stepValue=tempnum%10;
+            result+=stepValue;
             tempnum/=10;
-
         }
 
-        if(result==num)
-        {
-            System.out.println(num+" is Armstrong number.");
-        }
-
-        else System.out.println(num+" isn't Armstrong number.");
-
-
+        System.out.println("Sum of digits of the number: "+result);
     }
 }
