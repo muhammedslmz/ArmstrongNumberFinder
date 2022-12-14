128db47db12e4462d2164ae32e01c1ea94f9903c
Muhammed Solmaz <muhammed.slmz@gmail.com>
2022-12-14 09:06:47 +0300

    Harmonic number finder working well.

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,35 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Scanner inp =new Scanner(System.in);
-        double n= inp.nextDouble();
-        double total=0;
-        for (int i=1; i<=n;i++)
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Enter the n: ");
+        int n= inp.nextInt();
+        for (int i =0;i<=n;i++)
         {
-            total+=i/n;
+
+            for (int j =0; j<(n-i);j++)
+            {
+                System.out.print(" ");
+            }
+            for(int y=0;y<(2*i+1);y++)
+            {
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
-        System.out.println(total);
+        for (int i=0; i<=n; i++)
+        {
+            for (int k=0;k<(i+1);k++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=0;j<(2*n-(2*i+1));j++)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
+        }
+
     }
 }
